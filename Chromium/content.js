64a016427b52f8f285bc89bffd04f5c12a94a7e5
Wikicrafter <achba@outlook.com>
2023-06-20 01:50:44 +0400

    more elements

--- Chromium/content.js
+++ Chromium/content.js
@@ -1,10 +1,14 @@
 function removeAds() {
-  const adSelectors = [
-    // Example ad selectors for common ad elements
-    ".ad-banner",
-    ".ad-container",
-    ".ad-wrapper"
-  ];
+ const adSelectors = [
+  // Example ad selectors for common ad elements
+  ".ad-banner",
+  ".ad-container",
+  ".ad-wrapper",
+  ".ad-video",
+  ".ad-popup",
+  "iframe[src*='ad']",
+  ".ad-native"
+];
 
   adSelectors.forEach(selector => {
     const ads = document.querySelectorAll(selector);
